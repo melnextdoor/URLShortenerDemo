75dad581db6c396a1b55a379c334d7c4717bd182
melnextdoor <mchan120@myseneca.ca>
2023-10-02 22:07:00 -0400

    Unit Test Changes

---
+++ src/utils/Util.js
@@ -0,0 +1,14 @@
+function validateUrl(value) {
+	var urlPattern = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]*\.[a-zA-Z0-9()]*\.([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)
+  if (value !== undefined && value.length > 0) {
+		if (!urlPattern.test(value)) {
+			throw new TypeError ('ArgumentException')
+		}
+		return true
+	}
+	else {
+		throw 'InvalidArgumentException'
+	}
+}
+
+module.exports = { validateUrl };
